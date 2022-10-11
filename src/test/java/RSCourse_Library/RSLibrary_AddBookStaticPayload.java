bb57dacfb83831bd16e9b4c6934ad288af63b92b
Karthik <vemulapalli.karthik@gmail.com>
2022-10-11 11:57:33 +0530

    RestAssured Project Updated

--- src/test/java/RSCourse_Library/RSLibrary_AddBookStaticPayload.java
+++ src/test/java/RSCourse_Library/RSLibrary_AddBookStaticPayload.java
@@ -1,11 +1,13 @@
 package RSCourse_Library;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class RSLibrary_AddBookStaticPayload {
 	
@@ -18,7 +20,7 @@ public class RSLibrary_AddBookStaticPayload {
 		try {
 			
 			String ProjectDirectory = System.getProperty("user.dir");
-			String AddBook_JsonFilePath = ProjectDirectory+"\\src\\test\\resources\\JsonFiles\\"+JsonFileName;
+			String AddBook_JsonFilePath = ProjectDirectory + "\\src\\test\\resources\\JsonFiles\\" + JsonFileName;
 			
 			String AddBook_StringResponse = new String(Files.readAllBytes(Paths.get(AddBook_JsonFilePath)));
 			
@@ -32,7 +34,13 @@ public class RSLibrary_AddBookStaticPayload {
 				AddBook_ResponseBody_BookID = AddBook_JsonResponse.getString("ID");
 				
 				System.out.println("ID - "+AddBook_ResponseBody_BookID);
-			
+				
+				String AddBook_ResponseJsonPath = ProjectDirectory + "\\src\\test\\resources\\JsonFiles\\RSLibrary_AddBookSchema.json";
+				File AddBook_ResponseSchema = new File(AddBook_ResponseJsonPath);  //Path of ResponseBody Schema
+				
+				//Import the static method matchesJsonSchema method from restassured.module.jsv from JsonSchemaValidator Class
+				AddBook_Response.then().assertThat().body(matchesJsonSchema(AddBook_ResponseSchema));
+				
 		} catch (IOException e) { 
 			System.out.println("JSON File Not Found Exception");
 		}	
